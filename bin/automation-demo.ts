d3388fc8467b7d711f23fa82824dd6b3728190f3
Loren <ypd0412@stanford.edu>
2023-02-13 12:58:00 -0800

    reboot

--- bin/automation-demo.ts
+++ bin/automation-demo.ts
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CiCdAwsPipelineDemoStack } from '../lib/ci-cd-aws-pipeline-demo-stack';
+import { AutomationDemoStack } from '../lib/automation-demo-stack';
 
 const app = new cdk.App();
-new CiCdAwsPipelineDemoStack(app, 'CiCdAwsPipelineDemoStack', {
+new AutomationDemoStack(app, 'AutomationDemoStack', {
   env: {
     account: "466040009172", 
     region: "us-west-2",
   }
 });
-
-app.synth();
